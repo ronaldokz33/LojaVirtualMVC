3550328b5f5f5046230810221eaee6548314431b
ronaldokz33 <ronaldokz_33@hotmail.com>
2015-03-20 11:24:07 -0300

    Paginacao

---
+++ LojaVirtualMVC.WEB/Controllers/VitrineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LojaVirtualMVC.Repositorio;
+namespace LojaVirtualMVC.WEB.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 3;
+        //
+        // GET: /Vitrine/
+        public ActionResult ListaProdutos(int Pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos
+                .OrderBy(p => p.Descricao)
+            .Skip((Pagina - 1) * ProdutosPorPagina)
+            .Take(ProdutosPorPagina);
+
+            return View(produtos);
+        }
+	}
+}
---
+++ LojaVirtualMVC.WEB/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using LojaVirtualMVC.WEB.Models;
+namespace LojaVirtualMVC.WEB.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int,string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+            for (int i = 0; i < paginacao.TotalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+                
+                tag.AddCssClass("btn btn-default");
+                if(i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn btn-primary");
+                }
+                resultado.Append(tag);
+                
+            }
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
